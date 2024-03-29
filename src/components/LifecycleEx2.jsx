ae7c5b8f92ffbd67e7543ceb03e674c940b41957
leeswungwon <leeswandy@naver.com>
2024-03-29 13:52:53 +0900

    f

---
+++ src/components/LifecycleEx2.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LifecycleEx2 = (props) => {
+    console.log('1,constructor call');
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default LifecycleEx2;
